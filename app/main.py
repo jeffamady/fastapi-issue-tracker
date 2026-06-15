09f852a2fedc2a78ae70ea122148523eb3222a47
JeffAmady <jeff02.jn@gmail.com>
2026-06-15 09:08:37 -0400

    Add timer middleware to measure request processing time

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from routes.issues import router as issues_router
+from middleware.timer import timer_middleware
 
 app = FastAPI()
+
+app.middleware("http")(timer_middleware)
 
 app.include_router(issues_router)
